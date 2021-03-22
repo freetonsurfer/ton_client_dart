cb30087b3cfb0b1f59614553af8507cbf880ae1e
freetonsurfer <freetonsurfer@gmail.com>
2021-03-22 09:41:21 +0300

    sdk 1.11.1

--- lib/src/utils_module.dart
+++ lib/src/utils_module.dart
@@ -3,8 +3,19 @@ part of 'tonsdkmodule.dart';
 class UtilsModule extends _TonSdkModule {
   UtilsModule(TonSdkCore core) : super(core);
 
-///Converts address from any TON format to any TON format
-Future<ResultOfConvertAddress> convert_address(ParamsOfConvertAddress params) async {
-final res = await _tonCore.request('utils.convert_address',params.toString());
- return ResultOfConvertAddress.fromMap(res);}
+  ///Converts address from any TON format to any TON format
+  Future<ResultOfConvertAddress> convert_address(
+      ParamsOfConvertAddress params) async {
+    final res =
+        await _tonCore.request('utils.convert_address', params.toString());
+    return ResultOfConvertAddress.fromMap(res);
+  }
+
+  ///Calculates storage fee for an account over a specified time period
+  Future<ResultOfCalcStorageFee> calc_storage_fee(
+      ParamsOfCalcStorageFee params) async {
+    final res =
+        await _tonCore.request('utils.calc_storage_fee', params.toString());
+    return ResultOfCalcStorageFee.fromMap(res);
+  }
 }
---
+++ test/micro_test.dart
@@ -0,0 +1,60 @@
+import 'package:ton_client_dart/ton_client_dart.dart';
+import 'package:test/test.dart';
+
+var CLIENT_DEFAULT_SETUP = {
+  'network': {
+    'server_address': 'https://main.ton.dev',
+    'message_retries_count': 5,
+    'message_processing_timeout': 40000,
+    'wait_for_timeout': 40000,
+    'out_of_sync_threshold': 15000,
+    'access_key': ''
+  },
+  'crypto': {'fish_param': ''},
+  'abi': {
+    'message_expiration_timeout': 40000,
+    'message_expiration_timeout_grow_factor': 1.5
+  }
+};
+
+void main() async {
+  var client = TonClient();
+  setUp(() async {
+    try {
+      await client.connect(CLIENT_DEFAULT_SETUP);
+    } catch (e) {
+      print('$e');
+    }
+  });
+
+  tearDown(() async {
+    await client.disconnect();
+  });
+
+  group('fast test', () {
+    test('version', () async {
+      final res = await client.version();
+      expect(res.version, equals('1.11.1'));
+    });
+
+    test('query', () async {
+      final query = await client.net.query_collection(ParamsOfQueryCollection(
+          collection: 'accounts',
+          result: 'id code_hash',
+          filter: {
+            'id': {
+              'eq':
+                  '0:10c7933a5bd18331b706a66f6a9a89c1747a591043330015332d88b27d7cbe73'
+            }
+          }));
+      expect(
+          query.result[0]['id'],
+          equals(
+              '0:10c7933a5bd18331b706a66f6a9a89c1747a591043330015332d88b27d7cbe73'));
+      expect(
+          query.result[0]['code_hash'],
+          equals(
+              '207dc560c5956de1a2c1479356f8f3ee70a59767db2bf4788b1d61ad42cdad82'));
+    });
+  });
+}
